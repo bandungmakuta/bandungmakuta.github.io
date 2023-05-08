eec27614b0d6e71082e6f4a5593fe2362352965f
Bandung Makuta <97177419+bandungmakuta@users.noreply.github.com>
2023-05-08 15:39:46 +0700

    Initial Commit

---
+++ application/views/link/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Link Bandung Makuta</title>
+
+    <!-- Tell the browser to be responsive to screen width -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/fontawesome-free/css/all.min.css">
+
+    <!-- Ionicons -->
+
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <!-- Tempusdominus Bbootstrap 4 -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+
+    <!-- iCheck -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <!-- JQVMap -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/jqvmap/jqvmap.min.css">
+
+    <!-- Theme style -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>dist/css/adminlte.min.css">
+
+    <!-- overlayScrollbars -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+
+    <!-- Daterange picker -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/daterangepicker/daterangepicker.css">
+
+    <!-- summernote -->
+
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/summernote/summernote-bs4.css">
+
+    <!-- Google Font: Source Sans Pro -->
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        .background-image {
+            /*		background-image: url(<?= base_url('assets/') ?>img/background/link-melty-2.jpg);*/
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            height: 100vh;
+
+            /*position: absolute;*/
+            /*transform: translate(-50%, -50%);*/
+        }
+    </style>
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '166126622865058');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=166126622865058&ev=PageView&noscript=1" />
+    </noscript>
+    <!-- End Meta Pixel Code -->
+
+</head>
+
+<!--<body class="" style="background-color: #ffd5d1; margin-top: 0;">-->
+
+<body class="background-image">
+
+    <div class="">
+
+        <!-- Main content -->
+
+        <section class="justify-content-between">
+
+            <div class="container-fluid col-md-5 mt-5">
+
+                <div class="col-12">
+
+                    <div class="row">
+
+                        <div class="col-4"></div>
+
+                        <div class="col-4">
+
+                            <img src="<?= base_url('assets/') ?>img/logo/logo-oleh-makuta.png" alt="Photo" style="width: 100%; margin-top: 0;"><br><br><br>
+
+                        </div>
+
+                        <div class="col-4"></div>
+
+                    </div>
+
+                </div>
+
+                <!-- <a href="https://bandungmakuta.mygostore.com/outlet-1-vdv/0aa08808-7810-1fc4-8178-152235500040" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank">Cashback Gopay 50% at Gostore</a> -->
+                <button type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" data-toggle="modal" data-target="#modal-menu"><i class="fas fa-tags"></i> Produk Makuta</button>
+                <button type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" data-toggle="modal" data-target="#modal-location"><img src="<?= base_url('assets/') ?>img/logo/google-maps-logo.png" alt="" height="30px"> Lokasi Bandung Makuta</button>
+                <a href="https://www.tiktok.com/@bandungmakuta" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em; " target='_blank'><img src="<?= base_url('assets/') ?>img/logo/Tiktok-logo-landscape1.png" alt="" height="30px"> </a>
+                <a href="https://instagram.com/bandungmakuta?igshid=NmE0MzVhZDY=" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em; " target='_blank'><img src="<?= base_url('assets/') ?>img/logo/instagram-landscape.png" alt="" height="30px"></a>
+                <a href="https://shopee.co.id/onlineshopmakuta?smtt=0.126516957-1660536238.10" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank"><img src="<?= base_url('assets/') ?>img/logo/shopee.png" alt="" height="30px"> Order Via Shopee</a>
+                <a href="https://www.tokopedia.com/officialbandungmakuta" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank"><img src="<?= base_url('assets/') ?>img/logo/tokopedia.png" alt="" height="25px"> Order Via Tokopedia</a>
+                <a href="https://api.whatsapp.com/send?phone=6289512507848&text=Halo%20Kaka,%20saya%20mau%20pesan%20makuta" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em; " target='_blank'><img src="<?= base_url('assets/') ?>img/logo/whatsapp-logo.png" alt="" height="30px"></i> Pemesanan via Whatsapp</a>
+                <a href="https://gofood.link/a/yM9hYjG" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank"><img src="<?= base_url('assets/') ?>img/logo/Gofood-logo.png" alt="" height="30px"> Order Via Gofood</a>
+                <a href="https://food.grab.com/id/in/restaurant/bandung-makuta---banteng/6-C3TKAK32FGNTRE?exp_src" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank"><img src="<?= base_url('assets/') ?>img/logo/grabfood-logo.png" alt="" height="20px"> Order Via Grabfood</a>
+                <!--<a href="https://www.bandungmakuta.com/" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target="_blank">www.bandungmakuta.com</a>-->
+                <!-- <button type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" data-toggle="modal" data-target="#modal-download"><img src="<?= base_url('assets/') ?>img/logo/download.png" alt="" height="30px"> Makuta Drive</button> -->
+                <a href="<?= base_url('drive'); ?>" type="button" class="btn btn-block btn-lg" style="font-family: 'Montserrat', sans-serif; color: #48443A; background-color: #eed3d8; border-radius: 1.5em;" target=""><img src="<?= base_url('assets/') ?>img/logo/download.png" alt="" height="20px"> Makuta Drive</a>
+
+            </div><!-- /.container-fluid -->
+
+            <!-- MODAL MENU -->
+            <div class="modal fade" id="modal-menu" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content" style="background-color: #FFFFFF;">
+                        <div class="modal-header text-center">
+                            <img src="<?= base_url('assets/') ?>img/logo/logo-oleh-makuta.png" alt="Photo" style="width: 30%;">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <h4 style="font-family: 'Montserrat', sans-serif; color: #603133;">MENU</h4>
+                            <img src="<?= base_url('assets/') ?>img/main/pricelist1.png" alt="Photo" style="width: 100%;">
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <span></span>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+
+            <!-- MODAL ALAMAT -->
+            <div class="modal fade" id="modal-location" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content" style="background-color: #eed3d8;">
+                        <div class="modal-header text-center">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <h4>Lokasi Outlet</h4><br>
+                            <div class="card bg-light">
+                                <div class="card-header text-muted border-bottom-0">
+                                    <h2 class="lead"><b>Bandung Makuta<br>Banteng</b></h2>
+                                </div>
+                                <div class="card-body pt-0">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <ul class="ml-4 mb-2 fa-ul text-muted">
+                                                <li class="medium"><span class="fa-li"><img src="<?= base_url('assets/') ?>img/logo/google-maps-logo.png" alt="" height="30px"></span> Jl. K.H. Ahmad Dahlan No.102, Burangrang, Kec. Lengkong, Kota Bandung, Jawa Barat 40262</li>
+                                            </ul>
+                                            <span></span>
+                                            <a href="https://maps.app.goo.gl/TmEoLRNvdtt59eHP8" type="button" class="btn btn-default" style="font-family: 'Montserrat', sans-serif;" target="_blank">Lihat Lokasi</a>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <span></span>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                    <!-- /.modal-content-->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+
+        </section>
+
+        <!-- /.content -->
+
+    </div>
+
+
+
+    <div class="lockscreen-footer text-center mt-4">
+
+        Copyright © 2023 <b><a href="http://bandungmakuta.com/" class="text-black">Bandung Makuta</a></b><br>
+
+        All rights reserved
+
+    </div>
+
+    </div>
+
+    <!-- ./wrapper -->
+
+
+
+    <!-- jQuery -->
+
+    <script src="<?= base_url('assets/') ?>plugins/jquery/jquery.min.js"></script>
+
+    <!-- jQuery UI 1.11.4 -->
+
+    <script src="<?= base_url('assets/') ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+
+    <!-- Bootstrap 4 -->
+
+    <script src="<?= base_url('assets/') ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- ChartJS -->
+
+    <script src="<?= base_url('assets/') ?>plugins/chart.js/Chart.min.js"></script>
+
+    <!-- Sparkline -->
+
+    <script src="<?= base_url('assets/') ?>plugins/sparklines/sparkline.js"></script>
+
+    <!-- JQVMap -->
+
+    <script src="<?= base_url('assets/') ?>plugins/jqvmap/jquery.vmap.min.js"></script>
+
+    <script src="<?= base_url('assets/') ?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+    <!-- jQuery Knob Chart -->
+
+    <script src="<?= base_url('assets/') ?>plugins/jquery-knob/jquery.knob.min.js"></script>
+
+    <!-- daterangepicker -->
+
+    <script src="<?= base_url('assets/') ?>plugins/moment/moment.min.js"></script>
+
+    <script src="<?= base_url('assets/') ?>plugins/daterangepicker/daterangepicker.js"></script>
+
+    <!-- Tempusdominus Bootstrap 4 -->
+
+    <script src="<?= base_url('assets/') ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Summernote -->
+
+    <script src="<?= base_url('assets/') ?>plugins/summernote/summernote-bs4.min.js"></script>
+
+    <!-- overlayScrollbars -->
+
+    <script src="<?= base_url('assets/') ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+    <!-- AdminLTE App -->
+
+    <script src="<?= base_url('assets/') ?>dist/js/adminlte.js"></script>
+
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
+    <script src="<?= base_url('assets/') ?>dist/js/pages/dashboard.js"></script>
+
+    <!-- AdminLTE for demo purposes -->
+
+    <script src="<?= base_url('assets/') ?>dist/js/demo.js"></script>
+
+</body>
+
+</html>
